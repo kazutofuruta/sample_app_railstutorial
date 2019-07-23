563c7e5b8625eee9ebcec301a8630e2c41a60f9e
kazuto furuta <19940718fk@gmail.com>
2019-07-23 06:23:15 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+	test "invaild signup information" do
+		get signup_path
+		assert_no_difference 'User.count' do
+		post signup_path, params: { user: { name: "",
+				      		email: "user@invalid",
+						password: "foo",
+						password_confirmation: "bar" } }
+		end	
+	assert_template "users/new"
+	assert_select 'form[action="/signup"]'
+
+end
+
+	test "validate signup information" do
+		get signup_path
+		assert_difference 'User.count', 1 do
+			post signup_path, params: { user: { name: "kazuto",
+				       			email: "kazuto@furuta.com",
+							password: "kazukazu",
+							password_confirmation: "kazukazu" } }
+		end
+		follow_redirect!
+		assert_template 'users/show'
+		assert_not flash.empty?
+	end
+end
