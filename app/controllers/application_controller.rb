fe51517d8fe3d257afb4a1dc72de22f86fd852b8
kazuto furuta <19940718fk@gmail.com>
2019-07-31 11:34:54 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  Rails.application.routes.default_url_options[:host] = "localhost:3000"###
+  Rails.application.routes.default_url_options[:host] = "intense-anchorage-62079.herokuapp.com"###
   protect_from_forgery with: :exception
   include SessionsHelper
 end
